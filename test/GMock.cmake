ee02a8bdc255abfb778acabfbb69018f9609042e
Igor Korsunov <elquendi@yandex.ru>
2017-05-15 03:29:53 +0300

    Change the way GMock incorporated in project and update GMock to 1.8
    New method downloads GMock and GTest at configuration time and
    and treats it as a prat of the project instead of external dependency.
    It's now a recommended way to add GTest and GMock to your project,
    see https://github.com/google/googletest/blob/master/googletest/README.md
    for details.

--- test/GMock.cmake
+++ test/GMock.cmake
@@ -1,46 +1,30 @@
-# CMake script for including Google Test framework to the project
+# Download and unpack googletest at configure time
+configure_file(GMockDownloadCmakeLists.cmake googletest-download/CMakeLists.txt)
+execute_process(
+        COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download
+)
+if(result)
+    message(FATAL_ERROR "CMake configuration step for googletest failed: ${result}")
+endif()
+execute_process(
+        COMMAND ${CMAKE_COMMAND} --build .
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download
+)
 
-#Required for Google test
-find_package(Threads)
+if(result)
+    message(FATAL_ERROR "Build step for googletest failed: ${result}")
+endif()
 
-# Enable ExternalProject CMake module
-include(ExternalProject)
+# Prevent GoogleTest from overriding our compiler/linker options
+# when building with Visual Studio
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-# Set default ExternalProject root directory
-set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/ThirdParty)
+# Add googletest directly to our build. This adds
+# the following targets: gtest, gtest_main, gmock
+# and gmock_main
+add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src ${CMAKE_BINARY_DIR}/googletest-build)
 
-ExternalProject_Add(
-    googlemock
-    URL http://googlemock.googlecode.com/files/gmock-1.7.0.zip
-    # Disable install step
-    INSTALL_COMMAND ""
-    # Wrap download, configure and build steps in a sсript to log output
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON)
-
-# Specify include dir
-ExternalProject_Get_Property(googlemock source_dir)
-set(GTEST_INCLUDE_DIR ${source_dir}/gtest/include)
-set(GMOCK_INCLUDE_DIR ${source_dir}/include)
-include_directories(${GTEST_INCLUDE_DIR})
-include_directories(${GMOCK_INCLUDE_DIR})
-
-# GTest Library
-ExternalProject_Get_Property(googlemock binary_dir)
-set(GTEST_LIBRARY_PATH ${binary_dir}/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
-set(GTEST_LIBRARY gtest)
-
-add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
-set_property(TARGET ${GTEST_LIBRARY} PROPERTY IMPORTED_LOCATION ${GTEST_LIBRARY_PATH} )
-
-add_dependencies(${GTEST_LIBRARY} googlemock)
-
-# GMock Library
-set(GMOCK_LIBRARY_PATH ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
-set(GMOCK_LIBRARY gmock)
-
-add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
-set_property(TARGET ${GMOCK_LIBRARY} PROPERTY IMPORTED_LOCATION ${GMOCK_LIBRARY_PATH} )
-
-add_dependencies(${GMOCK_LIBRARY} googlemock)
+include_directories("${gtest_SOURCE_DIR}/include" "${gmock_SOURCE_DIR}/include")
